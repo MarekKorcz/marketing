05f6c61a3f7a9395a2cf4e8c83c127da2ff833d7
MarkKorcz <mark.korcz@gmail.com>
2020-12-22 12:29:13 +0100

    restructuring project

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const path = require('path')
 require('dotenv').config()
 const PORT = process.env.PORT
 
-// Homepage Route
-app.get('/', (req, res) =>{
-    res.send("yolo")
-})
+app.set('view engine', 'ejs')
 
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')))
+const mainRoutes = require('./routes/main')
+app.use('/', mainRoutes)
+
+// // Set static folder
+// app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
