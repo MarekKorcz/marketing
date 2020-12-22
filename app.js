1a997a48ac855ecc6521e7aa470ddaf2ac48b3de
MarkKorcz <mark.korcz@gmail.com>
2020-12-22 13:07:48 +0100

    template site

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require('express')
 const app = express()
 
-const path = require('path')
-
-require('dotenv').config()
-const PORT = process.env.PORT
-
 app.set('view engine', 'ejs')
+
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
 
 const mainRoutes = require('./routes/main')
 app.use('/', mainRoutes)
 
-// // Set static folder
-// app.use(express.static(path.join(__dirname, 'public')))
-
+require('dotenv').config()
+const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
