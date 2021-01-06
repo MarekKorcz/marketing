b40cb4e6c413cbe94991ab12f06575090d8b09b1
MarkKorcz <mark.korcz@gmail.com>
2021-01-06 21:04:06 +0100

    some main page content drafts

--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,7 @@
 exports.home = (req, res) =>{
     res.render('home')
 }
+
+exports.social_media_pricing = (req, res) =>{
+    res.render('social_media_pricing')
+}
--- routes/main.js
+++ routes/main.js
@@ -4,4 +4,6 @@ const mainController = require('../controllers/main')
 
 router.get('/', mainController.home)
 
+router.get('/social-media-pricing', mainController.social_media_pricing)
+
 module.exports = router
