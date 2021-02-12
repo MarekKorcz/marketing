b5cb0fe5d6c74e83c3372078c0565fe1f9821c99
MarkKorcz <mark.korcz@gmail.com>
2021-02-12 20:57:30 +0100

    scaffold for blog articles

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,3 +1,7 @@
 exports.list = (req, res) =>{
     res.render('blog/list')
 }
+
+exports.why_copywriting = (req, res) =>{
+    res.render('blog/why-copywriting')
+}
--- routes/blog.js
+++ routes/blog.js
@@ -4,4 +4,6 @@ const blogController = require('../controllers/blog')
 
 router.get('/', blogController.list)
 
+router.get('/why-copywriting', blogController.why_copywriting)
+
 module.exports = router
