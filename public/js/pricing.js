a25f69d86c17994de08271afb6688b7cefc8d264
MarkKorcz <mark.korcz@gmail.com>
2021-02-19 00:15:24 +0100

    pricing scaffold

--- public/js/pricing.js
+++ public/js/pricing.js
@@ -3,7 +3,22 @@ window.addEventListener('DOMContentLoaded', () => {
 
     for (let i = 0; i < listElements.length; i++) {
         listElements[i].addEventListener('click', (event) => {
-            console.log(event.target.parentElement)
+            if (event.target.parentElement.classList.contains('list-group-item')) {
+                enableOnlyOneListElementToBeActive(event.target.parentElement, listElements)
+            } else if (event.target.parentElement.classList.contains('d-flex')) {
+                enableOnlyOneListElementToBeActive(event.target.parentElement.parentElement, listElements)
+            } else if (event.target.parentElement.classList.contains('list-group')) {
+                enableOnlyOneListElementToBeActive(event.target, listElements)
+            }
         })
+    }
+
+    function enableOnlyOneListElementToBeActive(clickedElement, listElements) {
+        for (let j = 0; j < listElements.length; j++) {
+            if (listElements[j].classList.contains('active')) {
+                listElements[j].classList.remove('active')
+            }
+        }
+        clickedElement.classList.add('active')
     }
 })
