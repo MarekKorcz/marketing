1a0cf5144f15d9fd94513876ce08b6dae83a6729
MarkKorcz <mark.korcz@gmail.com>
2021-02-24 10:57:19 +0100

    adding contact and about pages

--- controllers/main.js
+++ controllers/main.js
@@ -5,3 +5,11 @@ exports.home = (req, res) =>{
 exports.pricing = (req, res) =>{
     res.render('pricing')
 }
+
+exports.about = (req, res) =>{
+    res.render('about')
+}
+
+exports.contact = (req, res) =>{
+    res.render('contact')
+}
--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const mainController = require('../controllers/main')
 
 router.get('/', mainController.home)
-
 router.get('/pricing', mainController.pricing)
+router.get('/about', mainController.about)
+router.get('/contact', mainController.contact)
 
 module.exports = router
