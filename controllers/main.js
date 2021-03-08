6e96a8b1a2ec6e0509480367e96aa2f294406563
MarkKorcz <mark.korcz@gmail.com>
2021-03-08 16:56:13 +0100

    temporary turn off pricing

--- controllers/main.js
+++ controllers/main.js
@@ -2,9 +2,9 @@ exports.home = (req, res) =>{
     res.render('home')
 }
 
-exports.pricing = (req, res) =>{
-    res.render('pricing')
-}
+// exports.pricing = (req, res) =>{
+//     res.render('pricing')
+// }
 
 exports.about = (req, res) =>{
     res.render('about')
--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const mainController = require('../controllers/main')
 
 router.get('/', mainController.home)
-router.get('/pricing', mainController.pricing)
+// router.get('/pricing', mainController.pricing)
 router.get('/about', mainController.about)
 router.get('/contact', mainController.contact)
 
